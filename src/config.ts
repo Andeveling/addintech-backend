0761c3419ec978310961ea4b9b3d111d9e93e75f
Andres Parra <andeveling@gmail.com>
2022-12-18 18:54:36 -0500

    fix/connection

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ const config = {
   MONGO_DB_NAME: process.env.MONGO_DB_NAME ?? 'addintech-db',
   MONGO_DB_USER: process.env.MONGO_DB_USER ?? 'admin',
   MONGO_DB_PASSWORD: process.env.MONGO_DB_PASSWORD ?? 'password',
-
+  MONGO_DB_URI: process.env.MONGO_DB_URI ?? `mongodb://localhost/addintech-db`,
   // JWT
   JWT_TOKEN: process.env.JWT_TOKEN ?? 'secret'
 };
--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', true);
 
 export const startDB = async () => {
   try {
-    const db = await mongoose.connect(`mongodb://${config.MONGO_DB_HOST}/${config.MONGO_DB_NAME}`, {});
+    const db = await mongoose.connect(config.MONGO_DB_URI, {});
 
     console.log(`database is connect to: ${db.connection.name}`);
   } catch (error) {
